18aa7ab41edd46da397b5efe49b49a3204a787fa
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-06-01 23:06:10 -0500

    - Create User Activity UI Setup

--- app/src/main/java/io/github/jgingh7/smack/LoginActivity.kt
+++ app/src/main/java/io/github/jgingh7/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package io.github.jgingh7.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginUserBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
